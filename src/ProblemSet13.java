9821abeecaa7c2455582c28f4a194ddc31a6be2e
Jonah Leibowitz <jonahleibowitz@comcast.net>
2021-03-10 22:24:02 -0500

    getting started

---
+++ src/ProblemSet13.java
@@ -0,0 +1,39 @@
+public class ProblemSet13 {
+
+    public static void main(String[] args){
+        int start = 0;
+        int[] numbers = {2, 4, 8};
+        int target = 0;
+
+        System.out.println(groupSum(start, numbers, target));
+    }
+
+
+    public static boolean groupSum(int start, int[] numbers, int target) {
+    
+    }
+/*
+    public static boolean groupSum6(int start, int[] numbers, int target) {
+
+    }
+
+    public static boolean groupNoAdj(int start, int[] numbers, int target) {
+
+    }
+
+    public static boolean groupSum5(int start, int[] numbers, int target) {
+
+    }
+
+    public static boolean groupSumClump(int start, int[] numbers, int target) {
+
+    }
+
+    public static boolean splitArray(int[] numbers) {
+
+    }
+
+    public static boolean splitOdd(int[] numbers) {
+
+    }*/
+}
